66a2511217180d5ed2e4c2160e1281b59ff92f56
naite.zhou <naite.zhou@mljr.com>
2020-06-01 19:20:50 +0800

    fix go mod

--- resp/resp_code.go
+++ resp/resp_code.go
@@ -3,5 +3,11 @@ package resp
 // 请求成功
 const SUCCESS = "0000"
 
+// 请求成功通用提示
+const SUCCESS_MSG = "success"
+
 // 未知错误
 const FAILED_UNKNOWN = "9999"
+
+// 请求失败通用提示
+const FAILED_UNKNOWN_MSG = "failed unknown"
