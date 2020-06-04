b2926d3fa070f231c6954c9746b5ca41d9befcbb
naite.zhou <naite.zhou@mljr.com>
2020-06-04 14:52:30 +0800

    add some new resp code

--- resp/resp_code.go
+++ resp/resp_code.go
@@ -6,8 +6,20 @@ const SUCCESS = "0000"
 // 请求成功通用提示
 const SUCCESS_MSG = "success"
 
+// -----------------通用错误------------------------
+
 // 未知错误
 const FAILED_UNKNOWN = "9999"
 
 // 请求失败通用提示
 const FAILED_UNKNOWN_MSG = "failed unknown"
+
+// -----------------代码报错，比如空指针之类的------------------------
+
+// dto映射失败
+const FAILED_DTO_DECODE = "5001"
+
+// dto映射失败提示
+const FAILED_DTO_DECODE_MSG = "数据映射失败"
+
+// -----------------常用业务错误------------------------
